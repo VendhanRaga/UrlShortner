4d0c1d55d7f385fb522eaf46ae0e7abc1f9a3da6
VendhanRaga <aeroragavendra@gmail.com>
2023-07-29 14:47:19 +0530

    Urlshortner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./Login/Login";
+import "./App.css";
+import Signup from "./Signup/Signup";
+import ForgotPassword from "./ForgotPassword/ForgotPassword";
+import ResetPassword from "./ResetPassword/ResetPassword";
+import Homepage from "./Homepage/Homepage";
+import ActivateAccount from "./Activation/ActivateAccount";
+import Shorten from "./ShortenPage/Shorten";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route path="/" exact={true} component={Login} />
+          <Route path="/create-account" exact={true} component={Signup} />
+          <Route
+            path="/activate-account/:token"
+            exact={true}
+            component={ActivateAccount}
+          />
+          <Route
+            path="/forgot-password"
+            exact={true}
+            component={ForgotPassword}
+          />
+          <Route
+            path="/reset-password/:token"
+            exact={true}
+            component={ResetPassword}
+          />
+          <Route path="/homepage" exact={true} component={Homepage} />
+          <Route path="/shorten" exact={true} component={Shorten} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
